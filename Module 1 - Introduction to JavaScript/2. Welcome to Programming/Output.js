9c6d9450aa821039ecc22573f8242a75e30e2fa0
Mirza Abbas <113203942+Mirza-Abbas@users.noreply.github.com>
2023-05-27 00:05:53 +0500

    Add files via upload

---
+++ Module 1 - Introduction to JavaScript/2. Welcome to Programming/Output.js
@@ -0,0 +1,5 @@
+console.log("%c Hello World!", "color: blue; font-size: 40px;");    // Use this command in the Browser's console to display the text in blue
+
+console.log("Hello" + "World!");
+
+console.log("Hello", "World!");
