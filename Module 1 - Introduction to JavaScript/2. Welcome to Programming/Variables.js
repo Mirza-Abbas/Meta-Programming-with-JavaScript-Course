8c049422ec03a86001197ea1b7ce9f6385acd66a
Mirza Abbas <113203942+Mirza-Abbas@users.noreply.github.com>
2023-05-28 00:07:19 +0500

    Add files via upload

---
+++ Module 1 - Introduction to JavaScript/2. Welcome to Programming/Variables.js
@@ -0,0 +1,20 @@
+'John'
+
+var person;
+
+var person = "John";
+
+console.log(person);
+
+person;
+
+console.log("Hello",person);
+
+var greeting = "Hello";
+
+console.log(greeting,person);
+
+greeting = "Hi";
+person = "Jack";
+
+console.log(greeting,person);
