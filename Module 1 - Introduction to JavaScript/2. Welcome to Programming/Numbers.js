80576abed45b66dd1ab8a99607ef937c72c0b19a
Mirza Abbas <113203942+Mirza-Abbas@users.noreply.github.com>
2023-05-29 00:01:58 +0500

    Added Numbers.js

---
+++ Module 1 - Introduction to JavaScript/2. Welcome to Programming/Numbers.js
@@ -0,0 +1,27 @@
+//run these commands in browser's console individually
+
+123
+
+123.456
+
+2+2
+
+2*2
+
+2-2
+
+2/2
+
+//Power 10**2 = 100
+10**2
+
+//Remainder 10%3 = 1 
+10%3
+
+10%2
+
+//Calculation order without parentheses
+2*4+8
+
+//Calculation order with parentheses
+2*(4+8)
