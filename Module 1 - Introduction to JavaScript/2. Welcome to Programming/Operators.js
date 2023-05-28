81d16ee57382b876b1e1060623c07087c4279c18
Mirza Abbas <113203942+Mirza-Abbas@users.noreply.github.com>
2023-05-29 00:01:32 +0500

    Add files via upload

---
+++ Module 1 - Introduction to JavaScript/2. Welcome to Programming/Operators.js
@@ -0,0 +1,17 @@
+//Arithmetic operations:
+console.log(2+2);
+
+console.log(1+2+3+4);
+
+console.log(20-18);
+
+console.log(2*3);
+
+console.log(8/1);
+
+//Comparison operations:
+console.log(3>2);
+
+console.log(2>3);
+
+console.log(10==10);
