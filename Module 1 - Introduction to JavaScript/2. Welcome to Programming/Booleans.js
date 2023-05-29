ac5761ee9b409f1cb008a5d0695ac5918861b655
Mirza Abbas <113203942+Mirza-Abbas@users.noreply.github.com>
2023-05-30 00:17:40 +0500

    Added Booleans.js

---
+++ Module 1 - Introduction to JavaScript/2. Welcome to Programming/Booleans.js
@@ -0,0 +1,20 @@
+//run these commands in browser's console individually
+
+1 < 2
+
+1 > 2
+
+1 == 2
+
+1 == '1'
+
+//Strict equality operator
+1 === '1'
+
+1 != 2
+
+1 != '1'
+
+//Strict inequality operator
+
+1 !== '1'
