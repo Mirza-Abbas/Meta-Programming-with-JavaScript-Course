35d59140ac19793269ac09ad085571aa0775efe7
Mirza Abbas <113203942+Mirza-Abbas@users.noreply.github.com>
2023-05-30 00:16:50 +0500

    Added Strings.js

---
+++ Module 1 - Introduction to JavaScript/2. Welcome to Programming/Strings.js
@@ -0,0 +1,18 @@
+//run these commands in browser's console individually
+
+//empty strings:
+''
+
+""
+
+//Non-empty strings:
+'abc!'
+
+"abc! 123/"
+
+//For apostrophe:
+"It's a lovely day"
+
+//For quotation marks:
+'"With great power comes great responsibility"'
+
