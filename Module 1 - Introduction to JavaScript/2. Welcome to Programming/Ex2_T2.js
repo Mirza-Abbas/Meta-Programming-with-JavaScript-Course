8518488b38b1f2f7e2e71116f5e80e283bce0045
Mirza Abbas <113203942+Mirza-Abbas@users.noreply.github.com>
2023-05-31 00:42:57 +0500

    Added Task 2 of Exercise 2

---
+++ Module 1 - Introduction to JavaScript/2. Welcome to Programming/Ex2_T2.js
@@ -0,0 +1,19 @@
+/*
+Task 2: Using the logical || operator
+
+Imagine you are coding a video game. Currently, you’re about to code some snippets related to the game over condition.
+
+Complete the task using the following steps:
+
+Declare the variable timeRemaining, and assign the value of 0 to it.
+
+Declare the variable energy, and assign the value of 10 to it.
+
+Console log the following parameters: "Game over: ", and timeRemaining == 0 || energy == 0
+*/
+
+var timeRemaining = 0;
+
+var energy = 10;
+
+console.log("Game over:",timeRemaining==0 || energy == 0);
