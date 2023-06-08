692afd6a4117484af8a63b433578c532d14673b6
Mirza Abbas <113203942+Mirza-Abbas@users.noreply.github.com>
2023-06-09 00:02:10 +0500

    Added Task 5 of Exercise 2

---
+++ Module 1 - Introduction to JavaScript/3. Conditionals and Loops/Ex2_T5.js
@@ -0,0 +1,16 @@
+/* 
+Task 5
+Write a "while" loop that will perform exactly the same repetitive code as this:
+
+console.log(2018)
+console.log(2019)
+console.log(2020)
+console.log(2021)
+console.log(2022)
+*/
+
+var year=2018;
+while(year<=2022){
+    console.log(year);
+    year++;
+}
