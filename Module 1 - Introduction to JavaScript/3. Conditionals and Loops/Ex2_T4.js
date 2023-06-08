4856d8656f5eac44ec07f2d304fabc4805e3ce71
Mirza Abbas <113203942+Mirza-Abbas@users.noreply.github.com>
2023-06-08 17:25:50 +0500

    Added Task 4 of Exercise 2

---
+++ Module 1 - Introduction to JavaScript/3. Conditionals and Loops/Ex2_T4.js
@@ -0,0 +1,18 @@
+/* 
+Task 4
+Write a "while" loop that will perform exactly the same repetitive code as this:
+
+console.log(5)
+console.log(4)
+console.log(3)
+console.log(2)
+console.log(1)
+console.log('Countdown finished!')
+*/
+
+var i=5;
+while(i>=1){
+    console.log(i);
+    i--;
+}
+console.log("Countdown finished!");
