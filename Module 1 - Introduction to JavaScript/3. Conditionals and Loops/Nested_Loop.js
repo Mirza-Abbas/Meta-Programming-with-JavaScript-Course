77d3fa4ff5aa7f2d642eafd8444499a78981d6ab
Mirza Abbas <113203942+Mirza-Abbas@users.noreply.github.com>
2023-06-11 00:07:01 +0500

    Added Nested_Loop.js

---
+++ Module 1 - Introduction to JavaScript/3. Conditionals and Loops/Nested_Loop.js
@@ -0,0 +1,10 @@
+console.log("Summer Holidays")
+
+for(var i = 2023; i < 2025; i++){
+    
+    console.log(i);
+    
+    for(var j = 5; j <= 7; j++ ){
+        console.log("--------"+j);
+    }
+}
