781a19c868d4faa9d18f4bf1713b58f61c99d462
Mirza Abbas <113203942+Mirza-Abbas@users.noreply.github.com>
2023-06-12 05:51:37 +0500

    Added Functions.js

---
+++ Module 2 - The Building Blocks of a Program/1. Array, Objects and Functions/Functions.js
@@ -0,0 +1,13 @@
+
+//Function declaration
+function addtwoNums(){
+
+    //Function body
+    var a = 10;
+    var b = 20;
+    var c = a + b;
+    console.log(c);
+}
+
+//Function Call
+addtwoNums();
