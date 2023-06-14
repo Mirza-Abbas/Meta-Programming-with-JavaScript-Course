6b8f30acdb14e36d3b77c83259bcf687b6196496
Mirza Abbas <113203942+Mirza-Abbas@users.noreply.github.com>
2023-06-15 00:05:58 +0500

    Added Object.js

---
+++ Module 2 - The Building Blocks of a Program/1. Array, Objects and Functions/Objects.js
@@ -0,0 +1,17 @@
+var Player = {};
+
+Player.Health = 50;
+Player.Energy = 30;
+Player.SpecialAbility = "Freeze Enemies for 2 seconds";
+
+var Enemy = {
+    Health : 20,
+    Energy : 10,
+    Movement : 3
+};
+
+Enemy.Defense = 5;
+
+console.log(Player);
+
+console.log(Enemy.Health);
