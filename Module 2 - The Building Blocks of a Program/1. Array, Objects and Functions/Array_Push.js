95493be07ed8a210269dcfb712b1f92d16dd64d3
Mirza Abbas <113203942+Mirza-Abbas@users.noreply.github.com>
2023-06-16 00:12:54 +0500

    Added Array_Push.js

---
+++ Module 2 - The Building Blocks of a Program/1. Array, Objects and Functions/Array_Push.js
@@ -0,0 +1,10 @@
+var fruits = ["apples"];
+
+console.log("Before push():");
+console.log(fruits);
+
+fruits.push("pears");
+fruits.push("bananas");
+
+console.log("After push():");
+console.log(fruits);
