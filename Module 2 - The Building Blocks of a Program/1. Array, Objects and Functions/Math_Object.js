b2aef1df382d0e92e8b573e54166498ea737ce69
Mirza Abbas <113203942+Mirza-Abbas@users.noreply.github.com>
2023-06-17 00:07:08 +0500

    Added Math_Object.js

---
+++ Module 2 - The Building Blocks of a Program/1. Array, Objects and Functions/Math_Object.js
@@ -0,0 +1,26 @@
+//Number Constants:
+console.log("PI Constant: " + Math.PI);
+console.log("Euler's Constant: " + Math.E);
+console.log("Natural Log of 2: " + Math.LN2);
+
+//Rounding Methods:
+console.log(Math.ceil(4.2));    //Rounds Up
+console.log(Math.floor(2.2));   //Rounds Down
+console.log(Math.round(7.5));   //Rounds Up if number >= 5
+console.log(Math.round(9.2));   //Rounds Down if number < 5
+
+console.log(Math.trunc(6.7));   //Removes Decimal
+
+//Arithmetic and Calculus Methods:
+console.log("Calculates Power:" + Math.pow(2,3));
+console.log("Calculates Square Root: " + Math.sqrt(16));
+console.log("Calculates Cube Root: " + Math.cbrt(8));
+console.log("Returns absolute value: " + Math.abs(-10));
+console.log("Logarithmic Method: " + Math.log(2));
+console.log("Logarithmic Method: " + Math.log2(2));
+console.log("Logarithmic Method: " + Math.log10(2));
+console.log("Returns Minimum Value: " + Math.min(2,8));
+console.log("Returns Maximum Value: " + Math.max(2,8));
+console.log("Trignometric Sin Method: " + Math.sin(7));
+console.log("Trignometric Cos Method: " + Math.cos(7));
+console.log("Trignometric Tan Method: " + Math.tan(7));
