f696232856b135d9147cdc75046480e134b03e6d
Mirza Abbas <113203942+Mirza-Abbas@users.noreply.github.com>
2023-06-18 11:51:47 +0500

    Added Math_Ceil.js

---
+++ Module 2 - The Building Blocks of a Program/1. Array, Objects and Functions/Math_Ceil.js
@@ -0,0 +1,21 @@
+
+//Round up any decimal number to the nearest integer
+Math.ceil();
+
+var rounded = Math.ceil(0.0001);
+console.log("0.0001 rounded to " + rounded);
+
+rounded = Math.ceil(0.5);
+console.log("0.5 rounded to " + rounded);
+
+rounded = Math.ceil(0.99);
+console.log("0.99 rounded to " + rounded);
+
+rounded = Math.ceil(1.01);
+console.log("1.01 rounded to " + rounded);
+
+rounded = Math.ceil(1.5);
+console.log("1.5 rounded to " + rounded);
+
+rounded = Math.ceil(2.99);
+console.log("2.99 rounded to " + rounded);
