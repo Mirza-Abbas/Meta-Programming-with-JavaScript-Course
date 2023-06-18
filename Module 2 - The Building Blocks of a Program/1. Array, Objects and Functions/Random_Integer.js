1e95d29e6ea176fe2e9c693a5ef78cfc787ced58
Mirza Abbas <113203942+Mirza-Abbas@users.noreply.github.com>
2023-06-18 11:52:16 +0500

    Added Random_Integer.js

---
+++ Module 2 - The Building Blocks of a Program/1. Array, Objects and Functions/Random_Integer.js
@@ -0,0 +1,9 @@
+
+//Get a random decimal number between 0 and 0.99
+//Multiply decimal number with 10
+var decimal = Math.random() * 10;
+
+//Round up the decimal number to get an integer between 1 and 10
+var rounded = Math.ceil(decimal);
+
+console.log(rounded);
