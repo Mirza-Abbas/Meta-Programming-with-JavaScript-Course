31e2723a58941c0a448a2d3dac50e764a829910d
Mirza Abbas <113203942+Mirza-Abbas@users.noreply.github.com>
2023-06-21 00:29:29 +0500

    Added typeOf_Operator.js

---
+++ Module 2 - The Building Blocks of a Program/1. Array, Objects and Functions/typeOf_Operator.js
@@ -0,0 +1,17 @@
+console.log(typeof(false));
+
+console.log(typeof(12));
+
+console.log(typeof("abc"));
+
+console.log(typeof([1,2,3]));
+
+console.log(typeof({First: 1}));
+
+console.log(
+    typeof(
+        function a(){ 
+            console.log("a");
+        }
+    )
+);
