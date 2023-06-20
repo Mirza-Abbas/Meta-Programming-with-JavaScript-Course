ab19ea22b619d68d6b6c55fdbff70efd67b4df12
Mirza Abbas <113203942+Mirza-Abbas@users.noreply.github.com>
2023-06-20 22:09:16 +0500

    String_Methods.js

---
+++ Module 2 - The Building Blocks of a Program/1. Array, Objects and Functions/String_Methods.js
@@ -0,0 +1,24 @@
+
+//.length method - returns length of string
+console.log("Hello".length);
+
+//.charAt method - returns individual character at a specific index
+console.log("Hello".charAt(1));
+
+//.concat method - joins two strings
+console.log("Wo".concat("rl").concat("d"));
+
+//.indexof method - returns index of first position that matches a character
+console.log("Hello".indexOf("l"));
+
+//.lastIndex method - returns index of last position that matches a character
+console.log("Hello".lastIndexOf("l"));
+
+//.split method - splits string into an array of sub-strings
+console.log("ho-ho-ho".split("-"));
+
+//.toUpperCase method - changes casing of strings to Uppercase
+console.log("Hello".toUpperCase());
+
+//.toLowerCase method - changes casing of strings to Lowercase
+console.log("Hello".toLowerCase());
