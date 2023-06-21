aa0aa37b9ab73864ea3f2e314cdee0779278c091
Mirza Abbas <113203942+Mirza-Abbas@users.noreply.github.com>
2023-06-22 00:44:28 +0500

    Delete Bug.js

--- Module 2 - The Building Blocks of a Program/Bug.js
+++
@@ -1,7 +0,0 @@
-//When a bug happens in a program it keeps running but in an unintended way.
-
-function addTwoNum(a, b){
-    console.log(a + b);
-}
-
-addTwoNum("A", 1);      //the function is intended to add two numbers but it still runs
