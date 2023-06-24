034e950dbcf2d3409bf73100b9ac898d811dacf9
Mirza Abbas <113203942+Mirza-Abbas@users.noreply.github.com>
2023-06-24 21:04:02 +0500

    Added Try_Catch.js

---
+++ Module 2 - The Building Blocks of a Program/2. Error Handling/Try_Catch.js
@@ -0,0 +1,10 @@
+try{
+    console.log(a + b);
+}catch(Err_Obj){
+    console.log("An Error Occurred");
+
+    //To Output Error:
+    //console.log(Err_Obj);
+}
+
+console.log("My Program is still running");
