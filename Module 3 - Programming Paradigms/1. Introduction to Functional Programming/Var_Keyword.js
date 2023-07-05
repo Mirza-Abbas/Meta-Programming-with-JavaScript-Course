689346fb6d807582a3413fb0ca5919ebff693b9d
Mirza Abbas <113203942+Mirza-Abbas@users.noreply.github.com>
2023-07-06 00:18:44 +0500

    Added Var_Keyword.js

---
+++ Module 3 - Programming Paradigms/1. Introduction to Functional Programming/Var_Keyword.js
@@ -0,0 +1,18 @@
+// 1. Can be accessed before declaration:
+console.log(user);
+
+var user = "a";
+
+console.log(user);
+
+
+// 2. Can be redeclared:
+
+var user = "b";
+
+console.log(user);
+
+
+var user = "c";
+
+console.log(user);
