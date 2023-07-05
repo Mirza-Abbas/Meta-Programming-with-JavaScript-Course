237ddb08a6f1e8b53eb9381db5e7d16831dfe4c8
Mirza Abbas <113203942+Mirza-Abbas@users.noreply.github.com>
2023-07-06 00:19:15 +0500

    Added Let_Keyword.js

---
+++ Module 3 - Programming Paradigms/1. Introduction to Functional Programming/Let_Keyword.js
@@ -0,0 +1,24 @@
+//Uncomment each part individually to run
+
+// 1. Can't be used before declaration:
+// console.log(user);
+
+// let user = "a";
+
+// Output: Reference Error
+
+// 2. Can't be redeclared:
+// let user = "a";
+
+// let user = "b";
+
+// Output: Syntax Error
+
+// 3. Can be reassigned:
+let user = "a";
+
+console.log(user);
+
+user = "b";
+
+console.log(user);
