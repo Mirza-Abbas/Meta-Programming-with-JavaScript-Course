e9c3853a0c5aa3ec74371b90c685ac7a0267b6b5
Mirza Abbas <113203942+Mirza-Abbas@users.noreply.github.com>
2023-07-07 00:52:02 +0500

    Added Const_Keyword.js

---
+++ Module 3 - Programming Paradigms/1. Introduction to Functional Programming/Const_Keyword.js
@@ -0,0 +1,20 @@
+//Uncomment each part individually to run
+
+// 1. Must be initialized:
+// const user;
+
+// Output: Syntax Error
+
+// 2. Can't be used before initialization:
+// console.log(user);
+
+// const user = "a";
+
+// Output: Reference Error
+
+// 3. Can't be redeclared or reassigned:
+// const user = "a";
+
+// user = "b";
+
+// Output: Type Error
