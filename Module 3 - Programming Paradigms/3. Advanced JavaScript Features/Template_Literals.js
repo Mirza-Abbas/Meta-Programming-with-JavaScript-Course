d302a124a9bb6d6ccf8fffd196f9bf8453e31124
Mirza Abbas <113203942+Mirza-Abbas@users.noreply.github.com>
2023-07-18 00:19:13 +0500

    Added Template_Literals.js

---
+++ Module 3 - Programming Paradigms/3. Advanced JavaScript Features/Template_Literals.js
@@ -0,0 +1,16 @@
+let greet = "Hello";
+let place = "World";
+
+console.log(`${greet} ${place}!`);
+
+console.log(`Hello
+World!`);
+
+console.log(`${2+3} stars!`);
+
+function marks(){
+    return Math.random() * 100;
+}
+
+
+console.log(`${marks() >= 50 ? 'Pass':'Fail'}`);
