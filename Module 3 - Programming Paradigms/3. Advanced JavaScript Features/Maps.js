78a7b95ffafd0932cc86a7267b11d0927585b8a0
Mirza Abbas <113203942+Mirza-Abbas@users.noreply.github.com>
2023-07-20 14:23:01 +0500

    Added Maps.js

---
+++ Module 3 - Programming Paradigms/3. Advanced JavaScript Features/Maps.js
@@ -0,0 +1,9 @@
+let Capitals = new Map();
+
+Capitals.set('China', "Beijing");
+Capitals.set("USA", "Washington");
+Capitals.set("Japan", "Tokyo");
+
+console.log(Capitals);
+
+console.log(Capitals.get('USA'));
