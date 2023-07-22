ffcb37ee62f8f7e8f7c4209c8505380f6df11e0e
Mirza Abbas <113203942+Mirza-Abbas@users.noreply.github.com>
2023-07-22 12:22:17 +0500

    Added Array_Concatenation.js

---
+++ Module 3 - Programming Paradigms/3. Advanced JavaScript Features/Array_Concatenation.js
@@ -0,0 +1,10 @@
+let fruits = ['apple', 'pear', 'plum'];
+
+let berries = ['blueberry', 'strawberry'];
+
+let fruitsAndBerries = [
+    ...fruits, 
+    ...berries
+];
+
+console.log(fruitsAndBerries); 
