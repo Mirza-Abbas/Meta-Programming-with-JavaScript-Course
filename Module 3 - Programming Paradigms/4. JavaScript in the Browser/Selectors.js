f2be029264b907c6c49d18599185ffd00de62865
Mirza Abbas <113203942+Mirza-Abbas@users.noreply.github.com>
2023-07-27 10:32:50 +0500

    Added Selectors.js

---
+++ Module 3 - Programming Paradigms/4. JavaScript in the Browser/Selectors.js
@@ -0,0 +1,11 @@
+//run these commands in Console Tab of Browser's DevTools
+
+console.log(document);
+
+console.log(document.querySelector('p'));
+
+console.log(document.querySelectorAll('p'));
+
+console.log(document.getElementById('heading'));
+
+console.log(document.getElementByClassName('txt'));
